2f0c2d7f69485f6d51bb787061255765ac00d3be
heterhum <xoxarc@gmail.com>
2024-02-24 11:47:02 +0100

    nouveau programme

---
+++ generateur-aleatoire.py
@@ -0,0 +1,28 @@
+import random
+
+nombre_repete_par_ordi = 0 #mise en place de chaque variable
+a = 0
+b = 0
+
+a = int(input("choisix l'étendue début: \n")) #demande l'étendue 
+b = int(input("choisix l'étendue fin: \n"))
+
+decompte_des_nombre ={i: 0 for i in range(a, b + 1)} #crée le dictionnaire qui sera des nombre comprit entre a et b, b inclue
+
+def choixal():                                #génération d'un nombre aléatoire entre a et b, b inclue
+    nombre_aleatoire = random.randint(a,b)
+    return nombre_aleatoire                   #permet l'accès a cette variable a la suite du programme
+
+utilisateur_choix = int(input("choisi le nombre de fois que tu veut que ce soit répété : \n"))         #nombre de fois ou le tout sera répété
+
+while nombre_repete_par_ordi < utilisateur_choix :         #tant que le programme ne la pas répété le meme nombre de fois demandé il choisie un nombre aléatoire, il ajoute 1 au nombre de fois ou
+    nombre_aleatoire = choixal()                           #le programme c'est répété et ajoute 1 a la valeur correspondant au nombre aléatoire choisie
+    nombre_repete_par_ordi = nombre_repete_par_ordi + 1
+    decompte_des_nombre[nombre_aleatoire] += 1
+else :
+    print(decompte_des_nombre)                   #donne tout le dictionnaire
+
+    
+
+
+
---
+++ math-de-tete.py
@@ -0,0 +1,32 @@
+import random
+import time
+
+start_time = time.time() #prise du temps au démmarage
+
+def choix() :
+    opération = ['*','-','+'] #définie une liste d'opérateur
+    a = random.randint(1, 300) #choisie un nombre aléatoire 
+    b = random.randint(1, 300) 
+    opérateur = random.choice(opération) #choisie un opérateur aléatoire de la liste définie avant
+    return a, b, opérateur
+a, b, opérateur = choix()
+
+while a < b and opérateur == '-' :
+    a, b, opérateur = choix()
+
+valeur_utilisateur = int(input(f'{a} {opérateur} {b} =\n')) #demande a l'utilisateur de rentré une valeur en ayant prit soin de mettre ce qu'on demande avant en int et faire un retour a la ligne avec \n
+
+temps_passé = time.time() - start_time #prend la temps écoulé aprés la réponse de l'utilisateur
+
+resultat = eval(f'{a} {opérateur} {b}') #definie le résultat attendue
+
+if valeur_utilisateur == resultat:                   #vérifie si la valeur entré est la meme que le résultat si oui alors il donne le temps et marque vrai sinon il marque faux
+    print(True)
+    print (f"Temps passé : {temps_passé:.2f} secondes") 
+else:
+    print(False)
+    print(resultat)
+    print (f"Temps passé : {temps_passé:.2f} secondes") 
+
+
+
