1883a06be0ea29eaa6c2181b9ad60a47bfe26407
heterhum <xoxarc@gmail.com>
2024-03-13 18:34:21 +0100

    finit !

--- simulateur-concours.py
+++ simulateur-concours.py
@@ -3,18 +3,19 @@ import random
 nombre_personne = 0
 personne = []
 
+continu = input("tapé *oui* quand vous voudrez commencé la tombola et *non* quand vous voulez la finir\n").lower()
 
-continu = input("d'autre personne doivent s'inscrire ? \n").lower()
-if continu == "true" :
-    utilisateur_incription = input("donnez votre nom \n")
-    personne.append(utilisateur_incription)
-    while continu == "true":
-        nombre_personne += 1
-        continu = input("D'autres personnes doivent-elles s'inscrire ? \n").lower()
-        if continu == "true":
-            utilisateur_inscription = input("Donnez votre nom\n")
-            personne.append(utilisateur_incription)
-
-print(len(personne),personne)
-
-
+while continu == "oui":
+    continu = input("Voulez vous vous inscrire ?\n").lower()
+    if continu == "oui":
+        utilisateur_inscription = input("Donnez votre nom\n")
+        personne.append(utilisateur_inscription)
+    elif continu != "non" :
+        print("je ne comprend pas, oui ou non")
+    if continu == "non" and len(personne) == 0:
+        print("erreur, il semble qu'il n'y est pas de personne a tiré au sort :(")
+        break
+    if continu == "non":
+        print("le gagnant est",random.choice(personne))
+        print(f"sur : {len(personne)}\nqui sont : {personne}")
+        break
