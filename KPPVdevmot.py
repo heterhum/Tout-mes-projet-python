804eb68468495e73174108a224064ec6b5c55e49
heterhum <160785637+heterhum@users.noreply.github.com>
2024-12-19 20:27:32 +0100

    petit con

--- KPPVdevmot.py
+++ KPPVdevmot.py
@@ -1,149 +1,5 @@
 #from math import sqrt
-#from tqdm.auto import tqdm
-#
-#print("démmare")
-#
-#file  = open("mots.txt","r",encoding="utf8")
-#lignes = file.readlines()
-#lignes=[l.strip('\n\r') for l in lignes]
-#l=[]
-#dico=[]
-#for i in lignes:
-#    dico.append("mot"+"-"+i)
-#    for j in range(30):
-#        if 0<=j<len(i):
-#            match i[j]:
-#                case "a": dico.append(i[j]+"02")
-#                case "b": dico.append(i[j]+"04")
-#                case "c": dico.append(i[j]+"06")
-#                case "d": dico.append(i[j]+"08")
-#                case "e": dico.append(i[j]+"10")
-#                case "f": dico.append(i[j]+"12")
-#                case "g": dico.append(i[j]+"14")
-#                case "h": dico.append(i[j]+"016")
-#                case "i": dico.append(i[j]+"018")
-#                case "j": dico.append(i[j]+"020")
-#                case "k": dico.append(i[j]+"022")
-#                case "l": dico.append(i[j]+"024")
-#                case "m": dico.append(i[j]+"026")
-#                case "n": dico.append(i[j]+"028")
-#                case "o": dico.append(i[j]+"030")
-#                case "p": dico.append(i[j]+"032")
-#                case "q": dico.append(i[j]+"034")
-#                case "r": dico.append(i[j]+"036")
-#                case "s": dico.append(i[j]+"038")
-#                case "t": dico.append(i[j]+"040")
-#                case "v": dico.append(i[j]+"042")
-#                case "w": dico.append(i[j]+"044")
-#                case "x": dico.append(i[j]+"046")
-#                case "y": dico.append(i[j]+"048")
-#                case "z": dico.append(i[j]+"050")
-#                case "â": dico.append(i[j]+"052")
-#                case "à": dico.append(i[j]+"054")
-#                case "ç": dico.append(i[j]+"056")
-#                case "é": dico.append(i[j]+"058")
-#                case "è": dico.append(i[j]+"060")
-#                case "-": dico.append(i[j]+"064")
-#                case "î": dico.append(i[j]+"066")
-#                case "û": dico.append(i[j]+"068")
-#                case "u": dico.append(i[j]+"070")
-#                case "ù": dico.append(i[j]+"071")
-#                case "ê": dico.append(i[j]+"072")
-#                case "ô": dico.append(i[j]+"073")
-#                case "ä": dico.append(i[j]+"074")
-#                case "ë": dico.append(i[j]+"075")
-#                case "ï": dico.append(i[j]+"076")
-#                case "ö": dico.append(i[j]+"077")
-#                case "ü": dico.append(i[j]+"078")
-#                case "ÿ": dico.append(i[j]+"079")
-#                case _: dico.append(i[j]+"080")
-#        else:
-#            dico.append(" "+"62")
-#    dico.append('0')
-#    l.append(dico)
-#    dico=[]
-#
-#print("démmare kppv")
-#
-#def distance_euclidienne(d2,d1):
-#    d2[-1]=int(d2[-1])
-#    b=list(zip(d1,d2))
-#    for i in range(1,30):
-#            d2[-1]+=(int(b[i][1][1:])-int(b[i][0][1:]))**2
-#    d2[-1]=sqrt(int(d2[-1]))
-#    return d2  
-#
-#def table_avec_distances(donnees, cible):
-#    for i in tqdm(range(len(donnees))):
-#        
-#        d=distance_euclidienne(donnees[i],cible)
-#        donnees[i]=d
-#    return donnees
-#
-#def tri_distance(d):
-#    d=sorted(d, key=lambda x: x[0])
-#    return d
-#
-#
-#def kppv(donnees, cible, k):    
-#    distances_voisins = table_avec_distances(donnees,cible)
-#    
-#    distances_voisins_triee = tri_distance(distances_voisins)
-#
-#    k_plus_proches_voisins = distances_voisins_triee[:k]
-#    
-#    return k_plus_proches_voisins
-#
-#mot="mat"
-#dico=[]
-#dico.append("mot"+"-"+mot)
-#for j in range(30):
-#    if 0<=j<len(mot):
-#        match i[j]:
-#            
-#            case "a": dico.append(i[j]+"02")
-#            case "b": dico.append(i[j]+"04")
-#            case "c": dico.append(i[j]+"06")
-#            case "d": dico.append(i[j]+"08")
-#            case "e": dico.append(i[j]+"10")
-#            case "f": dico.append(i[j]+"12")
-#            case "g": dico.append(i[j]+"14")
-#            case "h": dico.append(i[j]+"016")
-#            case "i": dico.append(i[j]+"018")
-#            case "j": dico.append(i[j]+"020")
-#            case "k": dico.append(i[j]+"022")
-#            case "l": dico.append(i[j]+"024")
-#            case "m": dico.append(i[j]+"026")
-#            case "n": dico.append(i[j]+"028")
-#            case "o": dico.append(i[j]+"030")
-#            case "p": dico.append(i[j]+"032")
-#            case "q": dico.append(i[j]+"034")
-#            case "r": dico.append(i[j]+"036")
-#            case "s": dico.append(i[j]+"038")
-#            case "t": dico.append(i[j]+"040")
-#            case "v": dico.append(i[j]+"042")
-#            case "w": dico.append(i[j]+"044")
-#            case "x": dico.append(i[j]+"046")
-#            case "y": dico.append(i[j]+"048")
-#            case "z": dico.append(i[j]+"050")
-#            case "â": dico.append(i[j]+"052")
-#            case "à": dico.append(i[j]+"054")
-#            case "ç": dico.append(i[j]+"056")
-#            case "é": dico.append(i[j]+"058")
-#            case "è": dico.append(i[j]+"060")
-#            case "-": dico.append(i[j]+"064")
-#            case "î": dico.append(i[j]+"066")
-#            case "û": dico.append(i[j]+"068")
-#    else:
-#        dico.append(" "+"62")
-#
-#
-#
-#c=kppv(l,dico,3)
-#print("mot cherché : ",  mot)
-#print("mot trouvé : ")
-#for i in c:
-#    print(i[0][4:])
+from tqdm.auto import tqdm
 
 print("démmare")
 
@@ -151,12 +7,11 @@ file  = open("mots.txt","r",encoding="utf8")
 lignes = file.readlines()
 lignes=[l.strip('\n\r') for l in lignes]
 
-mot="orthografe"
+mot="ortografe"
 trie=[]
 
-for i in lignes:
+for i in tqdm(lignes):
     s=len(i)-len(mot)
-    if s<0: continue
     n=len(i)
     c=[]
     for k in range(n):
@@ -164,7 +19,8 @@ for i in lignes:
         for j in z:
             if j[0]!=j[1]:
                 s+=1
-        c.append(s)
+        if s>=0:
+            c.append(s)
     s=len(i)-len(mot)
     for k in range(n):
         d=i
@@ -174,9 +30,11 @@ for i in lignes:
         for j in z:
             if j[0]!=j[1]:
                 s+=1
-        c.append(s)
-    c=sorted(c)[0]
-    trie.append([i,c])
+        if s>=0: 
+            c.append(s)
+    if c!=[]:
+        c=sorted(c)[0]
+        trie.append([i,c])
 
 trie=sorted(trie, key=lambda x: x[1])
 print(trie[:5])
--- snake.py
+++ snake.py
@@ -1,1 +1,173 @@
-#no
+import pygame
+
+N=10
+STEP=70
+
+screen = pygame.display.set_mode((N*STEP, N*STEP))
+running=True
+
+
+class snake():
+    """
+    move act : 1 = UP, 2 = DOWN, 3 =LEFT, 4 = RIGHT
+    """
+    def __init__(self):
+        self.mape=[[0 for x in range(N)] for y in range(N)]
+        self.move=[(1,0),(0,1),(0,-1),(-1,0)]
+        self.mape[5][5]=1
+        self.mov_act=3
+        self.longueur=1
+        self.keu=[[5,5],[5,5]]
+
+    def maj(self):
+        self.mape[self.keu[0][1]][self.keu[0][0]]=0
+        self.mape[self.keu[-1][1]][self.keu[-1][0]]=1
+        #for i in self.keu:
+        #    self.mape[i[-1]][i[0]]=1
+
+    def check(self):
+        if 0<=self.keu[-1][0]<=N and 0<=self.keu[-1][1]<=N:
+            return True
+        else:
+            self.mape=None
+            return False
+
+    def right(self):
+        n=[]
+        for i,j in zip(self.keu[-1],self.move[0]):
+            n.append(i+j)
+        if len(self.keu)-1==self.longueur:
+            self.keu[-1].append(n)
+            self.keu.pop(0)
+        else:
+            self.keu[-1].append(n)
+        self.move_act=4
+        if self.check():
+            self.maj()
+            return None
+        
+
+    def left(self):
+        n=[]
+        for i,j in zip(self.keu[-1],self.move[3]):
+            n.append(i+j)
+        if len(self.keu)-1==self.longueur:
+            self.keu[-1].append(n)
+            self.keu.pop(0)
+        else:
+            self.keu[-1].append(n)
+        self.move_act=3
+        if self.check():
+            self.maj()
+            return None
+    
+    def up(self):
+        n=[]
+        for i,j in zip(self.keu[-1],self.move[2]):
+            n.append(i+j)
+        if len(self.keu)-1==self.longueur:
+            self.keu[-1].append(n)
+            self.keu.pop(0)
+        else:
+            self.keu[-1].append(n)
+        self.move_act=1
+        if self.check():
+            self.maj()
+            return None
+    
+    def down(self):
+        n=[]
+        for i,j in zip(self.keu[-1],self.move[1]):
+            n.append(i+j)
+        if len(self.keu)-1==self.longueur:
+            self.keu[-1].append(n)
+            self.keu.pop(0)
+        else:
+            self.keu[-1].append(n)
+        self.move_act=2
+        if self.check():
+            self.maj()
+            return None
+        
+    def moove(self):
+        match self.mov_act:
+            case 1: self.up()
+            case 2: self.down()
+            case 3: self.left()
+            case 4: self.right()
+
+snakee=snake()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+        if event.type == pygame.KEYDOWN:
+            match event.key:
+                case pygame.K_UP: snakee.mov_act=1 
+                case pygame.K_DOWN: snakee.mov_act=2 
+                case pygame.K_RIGHT: snakee.mov_act=4 
+                case pygame.K_LEFT: snakee.mov_act=3  
+    pygame.time.wait(250)
+    snakee.moove()
+    if snakee.mape!=None:
+        for y in range(N):
+            for x in range(N):
+                if snakee.mape[y][x]==1:
+                    pygame.draw.rect(screen,(255,0,255),pygame.Rect(x*STEP, y*STEP, STEP, STEP))
+                    pygame.draw.rect(screen,(0,0,0),pygame.Rect(x*STEP, y*STEP, STEP, STEP),width=1,)
+                else:
+                    pygame.draw.rect(screen,(255,255,255),pygame.Rect(x*STEP, y*STEP, STEP, STEP))
+                    pygame.draw.rect(screen,(0,0,0),pygame.Rect(x*STEP, y*STEP, STEP, STEP),width=1,)
+    else :
+        running=False
+    pygame.display.update()
+    
+    
+    
+    
+    
+        #def move(m,tabl):
+    #    table=tabl.copy()
+    #    new=[]
+    #    table[keu[-1][1]][keu[-1][0]]=0
+    #    if m==1 and mov_act!=2: #down
+    #        #emplacement=(emplacement[0]-1,emplacement[1])
+    #        for i,j in zip(keu[-1],MOVE[m]):
+    #            new.append(i+j)
+    #        keu[-1]=new.copy()
+    #        if 0<=keu[-1][0]<=N and 0<=keu[-1][1]<=N:
+    #            table[keu[-1][1]][keu[-1][0]]=1
+    #            return table
+    #        else:
+    #            return None
+    #    if m==2 and mov_act!=1: #up
+    #        #keu[-1]=(keu[-1][0]-1,keu[-1][1])
+    #        for i,j in zip(keu[-1],MOVE[m]):
+    #            new.append(i+j)
+    #        keu[-1]=new.copy()
+    #        if 0<=keu[-1][0]<=N and 0<=keu[-1][1]<=N:
+    #            table[keu[-1][1]][keu[-1][0]]=1
+    #            return table
+    #        else:
+    #            return None
+    #    if m==3 and mov_act!=0: #left
+    #        #keu[-1]=(keu[-1][0]-1,keu[-1][1])
+    #        for i,j in zip(keu[-1],MOVE[m]):
+    #            new.append(i+j)
+    #        keu[-1]=new.copy()
+    #        if 0<=keu[-1][0]<=N and 0<=keu[-1][1]<=N:
+    #            table[keu[-1][1]][keu[-1][0]]=1
+    #            return table
+    #        else:
+    #            return None
+    #    if m==0 and mov_act!=3: #right
+    #        #keu[-1]=(keu[-1][0]-1,keu[-1][1])
+    #        for i,j in zip(keu[-1],MOVE[m]):
+    #            new.append(i+j)
+    #        keu[-1]=new.copy()
+    #        if 0<=keu[-1][0]<=N and 0<=keu[-1][1]<=N:
+    #            table[keu[-1][1]][keu[-1][0]]=1
+    #            return table
+    #        else:
+    #            return None
+    #    return tabl
