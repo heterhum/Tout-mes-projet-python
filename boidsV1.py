64d4ea992824d7d745a77ace775e7a3b793d320e
Heterhum <xoxarc@gmail.com>
2026-06-30 23:16:24 +0200

    lol that bad

--- boidsV1.py
+++ boidsV1.py
@@ -3,8 +3,8 @@ import numpy as np
 import random as rd
 import copy
  
-TAILLEX=500
-TAILLEY=500
+TAILLEX=1200
+TAILLEY=800
 
 screen = pygame.display.set_mode((TAILLEX,TAILLEY))
 clock=pygame.time.Clock()
@@ -29,7 +29,7 @@ def normalise(vect):
 class Pigeon:
     def __init__(self):
         self.NumberPig=150
-        self.NumberEat=1
+        self.NumberEat=2
         self.ListPig={}
         self.ListEat={}
 
@@ -39,6 +39,9 @@ class Pigeon:
         self.CohesionFactor=1
         self.EatFactor=12
 
+        self.Vision=10
+        self.EatVision=1000
+
     def CreatePig(self):
         for i in range(self.NumberPig):
             self.ListPig[i]={
@@ -46,17 +49,18 @@ class Pigeon:
                 "vit":30,
                 "vecDir": np.array([0.0,0.0]),
                 "vecSpeed":np.array([0.0,0.0]),
-                "vision":10, #if all same vision -> optimisation nieghPIg
+                "vision":self.Vision, #if all same vision -> optimisation nieghPIg
                 "sep":1,
                 "neigh":[],
                 "tosep":["n",float('inf')], #who, dist
-                "Eat":["n",float('inf')]
+                "Eat":["n",float('inf')],
+                "EatVision":self.EatVision,
                 }
         return
 
     def CreateEat(self):
         for i in range(self.NumberPig):
-            self.ListEat[i]=np.array([rd.randint(0,TAILLEX),rd.randint(0,TAILLEY)])
+            self.ListEat[i]=np.array([rd.randint(int(TAILLEX/4),int(TAILLEX/2)),rd.randint(int(TAILLEY/4),int(TAILLEY/2))])
 
     def DistancePig(self,n1,n2):
         dist=np.sqrt((self.ListPig[n1]["pos"][0]-self.ListPig[n2]["pos"][0])**2 + (self.ListPig[n1]["pos"][1]-self.ListPig[n2]["pos"][1])**2)
@@ -66,28 +70,31 @@ class Pigeon:
         dist=np.sqrt((self.ListPig[n1]["pos"][0]-self.ListEat[n2][0])**2 + (self.ListPig[n1]["pos"][1]-self.ListEat[n2][1])**2)
         return dist
 
-    def NeighPig(self):
-        for i in range(self.NumberPig):
-            for y in range(self.NumberPig):
-                if i!=y:
-                    d=self.DistancePig(i,y)
-                    if d<=self.ListPig[i]["vision"]:
-                        self.ListPig[i]["neigh"].append(y)
-                    if d<=self.ListPig[i]["sep"] and d<=self.ListPig[i]["tosep"][1]:
-                        self.ListPig[i]["tosep"][0]=y
-                        self.ListPig[i]["tosep"][1]=d
-            for y in range(self.NumberEat):
-                d=self.DistanceEat(i,y)
-                if d<=self.ListPig[i]["vision"] and d<=self.ListPig[i]["Eat"][1]:
-                    self.ListPig[i]["Eat"][1]=d
-                    self.ListPig[i]["Eat"][0]=y
+    def NeighPig(self,old,i):
+        for y in range(self.NumberPig):
+            if y>i:
+                d=self.DistancePig(i,y)
+                if d<=old[i]["vision"]:
+                    old[i]["neigh"].append(y)
+                    old[y]["neigh"].append(i)
+                if d<=old[i]["sep"] and d<=old[i]["tosep"][1]:
+                    old[i]["tosep"][0]=y
+                    old[i]["tosep"][1]=d
+                    old[y]["tosep"][0]=i
+                    old[y]["tosep"][1]=d
+        for y in range(self.NumberEat):
+            d=self.DistanceEat(i,y)
+            if d<=self.EatVision and d<=old[i]["Eat"][1]:
+                old[i]["Eat"][1]=d
+                old[i]["Eat"][0]=y
         return
 
     def Separation(self,n,tosep,old): #for now only in fonction of most near 
         if tosep[0]!="n":
             pri=old[n]["pos"]
             fuire=old[tosep[0]]["pos"]
-            vec=np.array([pri[0]-fuire[0],pri[1]-fuire[1]])
+            #vec=np.array([pri[0]-fuire[0],pri[1]-fuire[1]])
+            vec=pri-fuire
             Vedir=normalise(vec)
 
             return Vedir #direction to run from nearest pigeon
@@ -118,22 +125,27 @@ class Pigeon:
         if eat[0]!="n":
             pri=old[n]["pos"]
             fuire=self.ListEat[eat[0]]
-            vec=np.array([fuire[0]-pri[0],fuire[1]-pri[1]])
+            vec=fuire-pri
             Vedir=normalise(vec)
 
             return Vedir #direction to run from nearest pigeon
         else: return np.array([0.0,0.0])
 
-    def Mouvement(self):
-        for i in range(self.NumberPig):
-            self.ListPig[i]["pos"]=self.ListPig[i]["pos"]+(1/FPS)*self.ListPig[i]["vecSpeed"]
+    def Mouvement(self,i):
+        self.ListPig[i]["pos"]=self.ListPig[i]["pos"]+(1/FPS)*self.ListPig[i]["vecSpeed"]
         return
 
     def BIGPIGEON(self):
-        self.NeighPig()
         old=copy.deepcopy(self.ListPig)
         
         for i in range(self.NumberPig):
+            #if old[i]["Eat"][0]=="n" and int(old[i]["vecSpeed"][0])==0 and int(old[i]["vecSpeed"][1])==0: #badddd
+            #    old[i]["vision"]=1000
+            #    
+            #else:
+            #    old[i]["vision"]=self.Vision
+            self.NeighPig(old,i)
+
             vd0=self.Separation(i,old[i]["tosep"],old)
             vd1=self.Alignment(i,old[i]["neigh"],old)
             vd2=self.Cohesion(i,old[i]["neigh"],old)
@@ -156,24 +168,35 @@ class Pigeon:
 
             self.ListPig[i]["neigh"]=[]
             self.ListPig[i]["tosep"]=["n",float('inf')]
-            self.ListPig[i]["Eat"]=[0,float('inf')] #"n" not 0 ; it's a ~feature~
+            self.ListPig[i]["Eat"]=["n",float('inf')] #"n" not 0 ; it's a ~feature~
+
+            self.Mouvement(i)
+            self.Show(i)
+
+            for y in range(self.NumberEat):
+                if int(self.ListPig[i]["pos"][0])==self.ListEat[y][0] and int(self.ListPig[i]["pos"][1])==self.ListEat[y][1]:
+                    self.ListEat[y]=np.array([rd.randint(int(TAILLEX/4),int(TAILLEX/2)),rd.randint(int(TAILLEY/4),int(TAILLEY/2))])
+
         return
 
-    def Show(self):
+    def ShowEat(self):
         for i in range(self.NumberEat):
             posx=self.ListEat[i][0]
             posy=self.ListEat[i][1]
             if 0<=posx<=TAILLEX and 0<=posy<=TAILLEY:
+                #pygame.draw.circle(screen,(255,255,0),self.ListEat[i],self.EatVision)
+                #pygame.draw.circle(screen,(0,0,0),self.ListEat[i],self.EatVision-1)
                 pygame.draw.circle(screen,(255,255,0),self.ListEat[i],2)
-        for i in range(self.NumberPig):
-            posx=self.ListPig[i]["pos"][0]
-            posy=self.ListPig[i]["pos"][1]
-            if 0<=posx<=TAILLEX and 0<=posy<=TAILLEY:
-                p1=self.ListPig[i]["vecDir"]*3+self.ListPig[i]["pos"]
-                p2=[self.ListPig[i]["vecDir"][1]*1+self.ListPig[i]["pos"][0],-self.ListPig[i]["vecDir"][0]*1+self.ListPig[i]["pos"][1]]
-                p3=[-self.ListPig[i]["vecDir"][1]*1+self.ListPig[i]["pos"][0],self.ListPig[i]["vecDir"][0]*1+self.ListPig[i]["pos"][1]]
-                #pygame.draw.circle(screen,(255,0,0),self.ListPig[i]["pos"],2)
-                pygame.draw.polygon(screen,(255,0,0),[p1,p2,p3])
+
+    def Show(self,i):
+        posx=self.ListPig[i]["pos"][0]
+        posy=self.ListPig[i]["pos"][1]
+        if 0<=posx<=TAILLEX and 0<=posy<=TAILLEY:
+            p1=self.ListPig[i]["vecDir"]*3+self.ListPig[i]["pos"]
+            p2=[self.ListPig[i]["vecDir"][1]*1+self.ListPig[i]["pos"][0],-self.ListPig[i]["vecDir"][0]*1+self.ListPig[i]["pos"][1]]
+            p3=[-self.ListPig[i]["vecDir"][1]*1+self.ListPig[i]["pos"][0],self.ListPig[i]["vecDir"][0]*1+self.ListPig[i]["pos"][1]]
+            #pygame.draw.circle(screen,(255,0,0),self.ListPig[i]["pos"],2)
+            pygame.draw.polygon(screen,(255,0,0),[p1,p2,p3])
 
                 
 
@@ -190,10 +213,9 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos=pygame.mouse.get_pos()
             Pig.ListEat[0]=np.array([pos[0],pos[1]])
-            
+    Pig.ShowEat()        
     Pig.BIGPIGEON()
-    Pig.Mouvement()
-    Pig.Show()
+    
 
     pygame.display.update()
     clock.tick(FPS)
